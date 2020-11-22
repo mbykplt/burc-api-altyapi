502496cc539e0cb250e33bf78d5aac53be1e91c7
elywashere <36236673+mbykplt@users.noreply.github.com>
2020-11-22 21:32:49 +0300

    Add files via upload

--- models/burc.js
+++ models/burc.js
@@ -1,1 +1,12 @@
-
+const mongoose=require("mongoose");
+mongoose.connect("mongodb+srv://mbykplt:63120012@burclar.bikza.mongodb.net/<dbname>?retryWrites=true&w=majority",{ useNewUrlParser: true });
+const burclar= mongoose.Schema({
+  _id:mongoose.Schema.Types.ObjectId,
+  burcAdi:String,
+  tarih:String,
+  motto:String,
+  gezegen:String,
+  element:String,
+  yorum:String
+})
+module.exports=mongoose.model("burclar",burclar)
